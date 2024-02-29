8dee6a1b77ca7e71ab0b7dfdef4b747af1baeb02
danilom <danilom@gmail.com>
2024-03-01 00:49:36 +0700

    Brain module with updated docs

--- ports/rp2/modules/brain.py
+++ ports/rp2/modules/brain.py
@@ -1,10 +1,14 @@
+# pylint: disable line-too-long
+"""
+API for the JPO Brain and connected devices.
+"""
 import _jpo
 
 class ImuQuaternion:
     """
     Orientation as a quaternion.
     """
-    def __init__(self, i, j, k, real):
+    def __init__(self, i: float, j: float, k: float, real: float):
         self.i = i
         self.j = j
         self.k = k
@@ -19,7 +23,7 @@ class ImuAcceleration:
     """
     Linear acceleration (i.e., removing the effect of gravity) in [m/s^2].
     """
-    def __init__(self, x, y, z):
+    def __init__(self, x: float, y: float, z: float):
         self.x = x
         self.y = y
         self.z = z
@@ -33,7 +37,8 @@ class ColorReading:
     """
     Color reading from the color sensor.
     """
-    def __init__(self, clear, red, green, blue):
+    def __init__(self, clear: int, red: int, green: int, blue: int):
+        # TODO: document range. In C it's uint16
         self.clear = clear
         self.red = red
         self.green = green
@@ -50,171 +55,248 @@ METER_PER_SECOND_SQUARED = 1
 # IIC
 # ===
 class ImuSensor:
-    def __init__(self, iic_port):
+    """
+    Inertial Measurement Unit (IMU) sensor.
+    """
+    def __init__(self, iic_port: int):
         """
-        @param iic_port: IIC port the sensor is connected to [1-8]
+        Args:
+            iic_port: IIC port the sensor is connected to [1-8]
         """
         self._port = iic_port
         _jpo.iic_imu_init(self._port)
 
-    def poll_orientation(self):
+    def poll_orientation(self) -> ImuQuaternion:
         """
-        @return: an ImuQuaternion object with orientation data
+        Returns:
+            An ImuQuaternion object with orientation data
         """
         tup = _jpo.iic_imu_poll_orientation(self._port)
         #print("orientation tuple", tup)
         return ImuQuaternion(*tup)
 
-    def poll_acceleration(self, unit = METER_PER_SECOND_SQUARED):
+    def poll_acceleration(self, unit = METER_PER_SECOND_SQUARED) -> ImuAcceleration:
         """
         Polls the accelerometer for linear acceleration (i.e., removing the effect of gravity)
-        @param unit: unit of acceleration, METER_PER_SECOND_SQUARED 
-        @return: an ImuAcceleration object with acceleration data in [m/s^2]
+
+        Args:
+            unit: unit of acceleration, METER_PER_SECOND_SQUARED 
+        
+        Returns:
+            An ImuAcceleration object with acceleration data in [m/s^2]
         """
         tup = _jpo.iic_imu_poll_acceleration(self._port)
         return ImuAcceleration(*tup)
 
     def deinit(self):
+        """
+        Deinitialize the sensor.
+        """
         _jpo.iic_imu_deinit(self._port)
 
 class ColorSensor:
-    def __init__(self, iic_port):
+    """
+    Color detection sensor.
+    """
+    def __init__(self, iic_port: int):
         """
-        @param iic_port: IIC port the sensor is connected to [1-8]
+        Args:
+            iic_port: IIC port the sensor is connected to [1-8]
         """
         self._port = iic_port
         _jpo.iic_color_init(self._port)
 
-    def read(self):
+    def read(self) -> ColorReading:
         """
-        @return: a ColorReading object with detected color
+        Returns:
+            A ColorReading object with detected color
         """
         tup = _jpo.iic_color_read(self._port)
         #print("color_tuple", tup)
         return ColorReading(*tup)
-    
-    def set_led(self, red, green, blue, white):
+
+    def set_led(self, red: int, green: int, blue: int, white: int):
         """
         Set the color of the LED on the sensor.
+        Args:
+            red: the red component [0-255]
+            green: the green component [0-255]
+            blue: the blue component [0-255]
+            white: the white component [0-255]
         """
         # return is for testing
         return _jpo.iic_color_set_led(self._port, (red, green, blue, white))
 
     def deinit(self):
+        """
+        Deinitialize the sensor.
+        """
         _jpo.iic_color_deinit(self._port)
 
 class DistanceSensor:
-    def __init__(self, iic_port):
+    """
+    Ultrsonic distance sensor.
+    """
+    def __init__(self, iic_port: int):
         """
-        @param iic_port: IIC port the sensor is connected to [1-8]
+        Args:
+            iic_port: IIC port the sensor is connected to [1-8]
         """
         self._port = iic_port
         _jpo.iic_distance_init(self._port)
 
-    def read(self):
+    def read(self) -> float:
         """
-        @return: the distance (TODO: specify unit)
+        Returns:
+            the distance
         """
+        # TODO: specify unit
         return _jpo.iic_distance_read(self._port)
 
     def deinit(self):
+        """
+        Deinitialize the sensor.
+        """
         _jpo.iic_distance_deinit(self._port)
 
 # I/O
 # ===
 class Button:
-    def __init__(self, io_port):
+    """
+    Button with an on/off state on an IO port.
+    """
+    def __init__(self, io_port: int):
         """
-        @param io_port: IO port the button is connected to [1-11]
+        Args:
+            io_port: IO port the button is connected to [1-11]
         """
         self._port = io_port
         _jpo.io_button_init(self._port)
 
-    def is_pressed(self):
+    def is_pressed(self) -> bool:
         """
-        @return: True if the button is pressed, False otherwise
+        Returns:
+            True if the button is pressed, False otherwise
         """
         return _jpo.io_button_is_pressed(self._port)
-    
+
     # Micropython does not call __del__ on object destruction
     # See https://github.com/micropython/micropython/issues/1878
     def deinit(self):
+        """
+        Deinitialize the sensor.
+        """
         _jpo.io_deinit(self._port)
 
 class Output:
-    def __init__(self, io_port):
+    """
+    Digital output (on/off) on an IO port.
+    """
+    def __init__(self, io_port: int):
         """
-        @param io_port: IO port the output is connected to [1-11]
+        Args:
+            io_port: IO port the output is connected to [1-11]
         """
         self._port = io_port
         _jpo.io_output_init(self._port)
 
     def set(self, is_on):
         """
-        Set the output.
-        @param is_on: True to set, False to clear
+        Set the output on or off.
+
+        Args:
+            is_on: True to set, False to clear
         """
         _jpo.io_output_set(self._port, is_on)
 
     def deinit(self):
+        """
+        Deinitialize the sensor.
+        """
         _jpo.io_deinit(self._port)
 
 class Potentiometer:
-    def __init__(self, io_lower_port):
+    """
+    Potentiometer analog sensor.
+    """
+    def __init__(self, adc_io_port: int):
         """
-        @param io_lower_port: lower IO port the potentiometer is connected to [1-10]
-               For example, to set up an encoder on ports 4 and 5, pass 4 here.
+        Args:
+            adc_io_port: IO port the potentiometer is connected to [1-4]
+                Ports above 4 are not supported. 
         """
-        self._port = io_lower_port
+        self._port = adc_io_port
         _jpo.io_potentiometer_init(self._port)
 
-    def read(self):
+    def read(self) -> float:
         """
-        @return: the value of the potentiometer [0-100]
+        Returns:
+            Readout of the potentiometer [0-100]
         """
         return _jpo.io_potentiometer_read(self._port)
 
     def deinit(self):
+        """
+        Deinitialize the sensor.
+        """
         _jpo.io_deinit(self._port)
 
 class QuadratureEncoder:
-    def __init__(self, io_port):
+    """
+    Quadrature encoder sensor.
+    """
+    def __init__(self, io_lower_port):
         """
-        @param io_port: IO port the encoder is connected to [1-11]
+        Args:
+            io_lower_port: lower IO port the encoder is connected to [1-10]
+                Quadrature encoders use two consecutive IO ports.
+                For example, to set up an encoder on IO ports 4 and 5, pass 4 here.
         """
-        self._port = io_port
+        self._port = io_lower_port
         _jpo.io_encoder_init_quadrature(self._port)
 
-    def read(self):
+    def read(self) -> float:
         """
         @return: the value of the encoder [-100, 100]
         """
         return _jpo.io_encoder_read(self._port)
 
     def deinit(self):
+        """
+        Deinitialize the sensor.
+        """
         _jpo.io_deinit(self._port)
 
 # Motors
 # ======
 class Motor:
+    """
+    Motor connected to a motor port.
+    """
     def __init__(self, motor_port):
         """
-        @param motor_port: port the motor is connected to [1-10]
+        Args: 
+            motor_port: motor port the motor is connected to [1-10]
         """
         self._port = motor_port
         # Raises an error port is out of range (and stops the motor if running)
         _jpo.motor_set(self._port, 0)
 
-    def set_speed(self, speed):
+    def set_speed(self, speed: float):
         """
         Set the speed of the motor.
-        @param speed: the speed in percent [-100.0, 100.0]
+        Args: 
+            speed: the speed in percent [-100.0, 100.0]
         """
-        _jpo.motor_set(self._port, speed)        
+        _jpo.motor_set(self._port, speed)
 
 # Brain
 # =====
 class BrainButtons:
+    """
+    Buttons built into the Brain: up, down, enter, cancel.
+    """
+
     BTN_NONE = 0
     BTN_UP = 1 << 0
     BTN_DOWN = 1 << 1
@@ -224,91 +306,142 @@ class BrainButtons:
     def __init__(self):
         pass
 
-    def read(self):
+    def read(self) -> int:
+        """
+        Retrieve the state of all the buttons. 
+
+        Returns:
+            A bitfield indicating state of all the buttons. 
+            Check against BrainButtons.BTN_* constants.
+        
+        Example:
+            bb.read() & BrainButtons.BTN_UP == BrainButtons.BTN_UP
+        
+        Note:
+            For checking a single button, it's easier to use is_*_pressed() methods
+        """
         return _jpo.brain_get_buttons()
 
-    def is_up_pressed(self):
+    def is_up_pressed(self) -> bool:
+        """
+        Returns:
+            True if the up button is pressed, False otherwise
+        """
         return _jpo.brain_get_buttons() & BrainButtons.BTN_UP == BrainButtons.BTN_UP
-    
-    def is_down_pressed(self):
+
+    def is_down_pressed(self) -> bool:
+        """
+        Returns:
+            True if the down button is pressed, False otherwise
+        """
         return _jpo.brain_get_buttons() & BrainButtons.BTN_DOWN == BrainButtons.BTN_DOWN
-    
-    def is_cancel_pressed(self):
+
+    def is_cancel_pressed(self) -> bool:
+        """
+        Returns:
+            True if the cancel button is pressed, False otherwise
+        """
         return _jpo.brain_get_buttons() & BrainButtons.BTN_CANCEL == BrainButtons.BTN_CANCEL
-    
-    def is_enter_pressed(self):
+
+    def is_enter_pressed(self) -> bool:
+        """
+        Returns:
+            True if the enter button is pressed, False otherwise
+        """
         return _jpo.brain_get_buttons() & BrainButtons.BTN_ENTER == BrainButtons.BTN_ENTER
 
 class Oled:
+    """
+    OLED display built into the Brain. 
+    """
     def __init__(self):
         self.render_immediately = True
+        """
+        If True, the display is rendered immediately after each operation.
+        If False, the display is rendered only after calling render().
+        """
 
-    def set_pixel(self, x, y, is_on = True):
+    def set_pixel(self, x: int, y: int, is_on = True):
         """
         Set a pixel on the display.
-        @param x: the x coordinate
-        @param y: the y coordinate
-        @param is_on: True to set, False to clear
+        When working with individual pixels, for better performance set `render_immediately` to False.
+
+        Args:
+            x: the x coordinate [0-128]
+            y: the y coordinate [0-64]
+            is_on: True to set, False to clear
         """
+        # SSD1306_WIDTH, SSD1306_HEIGHT
         _jpo.oled_set_pixel(x, y, is_on)
-        if (self.render_immediately):
-            _jpo.oled_render()
-    
-    def clear_pixel(self, x, y):
-        """
-        Clear a pixel on the display.
-        @param x: the x coordinate
-        @param y: the y coordinate
-        """
-        self.set_pixel(x, y, False)
-    
-    def clear_row(self, row):
-        """ 
-        Clear a row of pixels
-        @param row: the row to clear [0-7]
-        """
-        _jpo.oled_clear_row(row)
-        if (self.render_immediately):
+        if self.render_immediately:
             _jpo.oled_render()
 
-    def write_at(self, row, column, *items):
+    def clear_pixel(self, x: int, y: int):
         """
-        Write a string to the display
-        @param row: the row to write to [0-7]
-        @param column: the column to write to [0-15]
-        @param items: items to write
+        Clear a pixel on the display.
+
+        Args:
+            x: the x coordinate [0-128]
+            y: the y coordinate [0-64]
+        """
+        self.set_pixel(x, y, False)
+
+    def clear_row(self, row: int):
+        """ 
+        Clear a row of characters.
+
+        Args:
+            row: the row to clear [0-7]
+        """
+        _jpo.oled_clear_row(row)
+        if self.render_immediately:
+            _jpo.oled_render()
+
+    def write_at(self, row: int, column: int, *items):
+        """
+        Write a string to the display.
+
+        Args:
+            row: the row to write to [0-7]
+            column: the column to write to [0-15]
+            items: items to write, any object, similar to built-in `print`
         """
         text = ' '.join([str(a) for a in items])
         _jpo.oled_printf(row, column, text)
-        if (self.render_immediately):
+        if self.render_immediately:
             _jpo.oled_render()
 
     def print(self, *items):
         """
-        Write a line to the display and scroll as needed
-        @param items: items to write
+        Write a line to the display and scroll as needed. Always renders immediately.
+
+        Args: 
+            items: items to write, any object, similar to built-in `print`
         """
         text = ' '.join([str(a) for a in items])
         _jpo.oled_printf_line(text)
         # always renders immediately
 
     def render(self):
+        """
+        Render the display.
+        """
         _jpo.oled_render()
-
-    # TODO: get/set buffer
-
-    # TODO: print with scrolling and line-clearing,
-    # similar to built-in print
-    # maybe separate into another class?
-    # we alrady have something like this in C,
-    # with DBG_OLED, maybe add that to hal API?
 
     def clear(self):
         """
-        Clear the display.
+        Clear the entire display.
         """
         _jpo.oled_clear()
-        if (self.render_immediately):
+        if self.render_immediately:
             _jpo.oled_render()
 
-        # TODO: if we have print, reset the line to top
+    def access_buffer(self) -> bytearray:
+        """
+        Access the internal display buffer.
+
+        Returns:
+            Bytearray representing the display buffer. Format is hardware specific. 
+            First byte is special, modify others to manipulate pixels. 
+        """
+        return _jpo.oled_access_buffer()
