cbe1fc8f79099fcfb96c2f76d224729b0ebce8c6
danilom <danilom@gmail.com>
2024-03-28 20:04:37 +0700

    Supporting usage of up to 16Mb of flash

--- ports/rp2/boards/JPO_BRAIN/mpconfigboard.cmake
+++ ports/rp2/boards/JPO_BRAIN/mpconfigboard.cmake
@@ -1,1 +1,17 @@
 # cmake file for Raspberry Pi Pico
+
+### Prerequisites
+if (NOT DEFINED ENV{JPO_PATH})
+	message(FATAL_ERROR "Environment variable JPO_PATH not set.")
+endif()
+# Fix backslashes in Windows
+file(TO_CMAKE_PATH "$ENV{JPO_PATH}" JPO_PATH)
+message("JPO_PATH (from environment) is ${JPO_PATH}")
+
+# Set board
+set(JPO_BOARD_DIR "${JPO_PATH}/resources/build_config")
+if (NOT EXISTS "${JPO_BOARD_DIR}")
+	message(FATAL_ERROR "Directory '${JPO_BOARD_DIR}' does not exist.")
+endif()
+set(PICO_BOARD_HEADER_DIRS "${JPO_BOARD_DIR}")
+set(PICO_BOARD "jpo_brain")
--- ports/rp2/boards/JPO_BRAIN/mpconfigboard.h
+++ ports/rp2/boards/JPO_BRAIN/mpconfigboard.h
@@ -1,3 +1,3 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "JPO Brain"
-#define MICROPY_HW_FLASH_STORAGE_BYTES          (1408 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (((14 * 1024) + 1408) * 1024)
