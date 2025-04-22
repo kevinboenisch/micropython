40368d6770c6f3e32a7e878cfd66902dffb985f3
danilom <danilom@gmail.com>
2025-04-22 23:03:35 +0700

    bootloader: adjusting load script to 56kb startup debugging

--- ports/rp2/main.c
+++ ports/rp2/main.c
@@ -109,19 +109,22 @@ void check_watchdog_flags() {
 }
 
 // DEBUG
-extern bool dbg_hal_init();
 #include "jpo/hal/oled.h"
 
 int main(int argc, char **argv) {
     // DEBUG
-    dbg_hal_init();
-    DBG_OLED("dbg 3 jcin");
+    hal_init();
+    DBG_OLED("main");
 
+    int i = 0;
     while(true) {
-        sleep_ms(1000);
+        DBG_OLED("loop %d", i++);
+        sleep_ms(3000);
     }
     return 0;
+}
 
+int rest_of_main(int argc, char **argv) {
     // This is a tickless port, interrupts should always trigger SEV.
     #if PICO_ARM
     SCB->SCR |= SCB_SCR_SEVONPEND_Msk;
